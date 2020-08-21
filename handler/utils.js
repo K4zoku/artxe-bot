9e669fde6cb625da7d0d3f561c880bbefaf0078f
takahatashun <takahatashunvn@gmail.com>
2020-08-21 10:45:12 +0700

    Update for heroku deploy

--- handler/utils.js
+++ handler/utils.js
@@ -20,6 +20,7 @@ module.exports = {
             })
             .catch(Logger.error);
     },
+
     numberWithCommas: (x=0) => {
         return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
     },
