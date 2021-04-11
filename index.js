2f9a677d580f11670edab49c66944842d9bfe7e4
takahatashun <takahatashunvn@gmail.com>
2021-04-11 13:28:39 +0700

    added web listener

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const http = require('http');
+
 (async () => {
 	console.log("Initializing...");
 	process.global = {
@@ -10,6 +12,10 @@
 			tty: require("./configuration/tty.json")
 		}
 	};
+	http.createServer((request, response) => {
+		response.writeHead(200, { 'Content-Type': "text/plain" });
+		response.end("success", 'utf-8');
+	}).listen(process.env.PORT || 4400);
 })()
 .then(() => require("./core/logger")()) // init logger
 .catch(console.error) // display error and stop when init logger failed
