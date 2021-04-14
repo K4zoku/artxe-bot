35d70183c22936fe340fada47c6488ce25552c20
takahatashun <takahatashunvn@gmail.com>
2021-04-14 19:36:56 +0700

    fixed

--- core/discord/autoload.js
+++ core/discord/autoload.js
@@ -1,18 +1,33 @@
+const {src, logger} = process.global;
 const {readdir} = require("fs").promises;
 const Event = require("./event");
 const {join} = require("path");
+const CommandManager = require(join(src, "core", "command", "command_manager"));
 
-const {src, logger} = process.global;
 
 module.exports = async () => require("./client")()
-	.then(client => loadEvents(client));
+	.then(client => {
+		loadCommands();
+		loadEvents(client);
+	});
 
 const eventDirectory = join(src, "core", "discord", "events");
-const loadEvents => async (client) {
+const loadEvents = async (client) => {
 	(await readdir(eventDirectory))
 		.filter(file => file.toLowerCase().endsWith(".js"))
         .map(file => join(eventDirectory, file))
         .map(require)
         .filter(object => object instanceof Event) // Event[]
         .forEach(event => event.register(client))
+}
+
+const loadCommands = async () => {
+	logger.info("Loading Discord commands...");
+    const discordCM = new CommandManager({
+    	writer: (content, message) => message.channel.send(content)
+    });
+    discordCM.loadCommands(join(src, "core", "discord", "commands"));
+    process.global.discord.command = {
+    	manager: discordCM
+    }
 }
--- core/discord/events/ready.js
+++ core/discord/events/ready.js
@@ -1,21 +1,8 @@
 const {join} = require("path");
 const Event = require("../event");
 const {src, logger, discord} = process.global;
-const CommandManager = require(join(src, "core", "command", "command_manager"));
 const client = discord.client;
 
 module.exports = new Event("ready", async () => {
 	logger.info(`Logged-in as ${client.user.tag}`);
-	await loadCommands();
 });
-
-async function loadCommands() {
-	logger.info("Loading Discord commands...");
-    const discordCM = new CommandManager({
-    	writer: (content, message) => message.reply(content)
-    });
-    discordCM.loadCommands(join(src, "core", "discord", "commands"));
-    process.global.discord.command = {
-    	manager: discordCM
-    }
-}
